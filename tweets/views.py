c2b7e185d782402b8e6aceb46fd47945d17a0b5c
middleverse <arshi_b@hotmail.com>
2020-12-20 12:19:14 -0500

    detail tweet view changed to return Json Response

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Tweet
 
@@ -9,10 +9,25 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("Hello Universe")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by Javascript
+    returns json data
+    """
+
     print(args)
     print(kwargs)
+    
+    data = {
+        'id' : tweet_id,
+        # 'image_path' = obj.image,
+    }
+    status = 200
     try:
         obj= Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f'Hello {tweet_id} - {obj.content} Universe')
+        data['message'] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status)
