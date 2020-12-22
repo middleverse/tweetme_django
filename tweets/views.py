ffb2994ffe4ceed3bc62b4dcb29627144b96244a
middleverse <arshi_b@hotmail.com>
2020-12-21 19:16:54 -0500

    create tweet redirect process checks for safe url

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,14 @@
 import random
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
+from django.utils.http import is_safe_url
 
 from .forms import TweetForm
 from .models import Tweet
+
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 def home_view(request, *args, **kwargs):
     return render(request,'pages/home.html', context={}, status=400)
@@ -18,7 +23,7 @@ def tweet_create_view(request, *args, **kwargs):
         obj = form.save(commit=False) # save returns an object that hasn't been saved to the DB yet
         # do other form related logic in between
         obj.save() # save the object to the database
-        if next_url != None:
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form" : form})
