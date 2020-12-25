83d723b1192670399e813bde8ecd618fe3d144e2
middleverse <arshi_b@hotmail.com>
2020-12-25 17:13:55 -0500

    added some fine testing methods

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -24,7 +24,7 @@ from tweets.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
-    path('tweets', tweet_list_view),
+    path('tweets/', tweet_list_view),
     path('create-tweet', tweet_create_view),
     path('api/tweets/', include('tweets.urls'))
 ]
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,96 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+from .models import Tweet
+
+User = get_user_model()
+
+class TweetTestCase(TestCase):
+    def setUp(self): # in built method that gets you set up for testing
+        self.user = User.objects.create_user(username='cfe', password='somepassword')
+        self.userb = User.objects.create_user(username='cfe_2', password='somepassword2')
+        
+        # if you want anything in the database, it has to go in this function
+        Tweet.objects.create(content='my first tweet', user=self.user)
+        Tweet.objects.create(content='my also first tweet', user=self.user)
+        Tweet.objects.create(content='my also second tweet', user=self.userb)
+        self.currentCount = Tweet.objects.all().count()
+
+
+    def test_tweet_created(self):
+        tweet_obj = Tweet.objects.create(content='my second tweet', user=self.user)
+        self.assertEqual(tweet_obj.id, 4)
+        self.assertEqual(tweet_obj.user, self.user)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='somepassword')
+        return client 
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get('/api/tweets/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 3)
+
+    def test_action_like(self):
+        client = self.get_client()
+        response = client.post('/api/tweets/action/', {'id': 1, 'action': 'like'})
+        self.assertEqual(response.status_code, 200)
+        like_count = response.json().get('likes')
+        self.assertEqual(like_count, 1)
+
+    def test_action_unlike(self):
+        client = self.get_client()
+        response = client.post('/api/tweets/action/', 
+            {'id': 2, 'action': 'like'})
+        self.assertEqual(response.status_code, 200)
+        response = client.post('/api/tweets/action/', 
+            {'id': 2, 'action': 'unlike'})
+        self.assertEqual(response.status_code, 200)
+        like_count = response.json().get('likes')
+        self.assertEqual(like_count, 0)
+
+    def test_action_retweet(self):
+        client = self.get_client()
+        response = client.post('/api/tweets/action/', 
+            {'id': 2, 'action': 'retweet'})
+        self.assertEqual(response.status_code, 201)
+        data = response.json()
+        new_tweet_id = data.get('id')
+        self.assertNotEqual(new_tweet_id, 2)
+        self.assertEqual(self.currentCount + 1, new_tweet_id)
+
+    def test_tweet_create_api_view(self):
+        client = self.get_client()
+        data =  {'content': 'created with post'}
+        response = client.post('/api/tweets/create/', data)
+        self.assertEqual(response.status_code, 201)
+        data = response.json()
+        new_tweet_id = data.get('id')
+        self.assertEqual(self.currentCount + 1, new_tweet_id)
+    
+    def test_tweet_detail_api_view(self):
+        client = self.get_client()
+        response = client.get('/api/tweets/1/')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        _id = data.get('id')
+        self.assertEqual(_id, 1)
+
+    def test_tweet_delete_api_view(self):
+        client = self.get_client()
+        response = client.delete('/api/tweets/1/delete/')
+        self.assertEqual(response.status_code, 200)
+
+        # page not found error (tweet already deleted)
+        response = client.delete('/api/tweets/1/delete/')
+        self.assertEqual(response.status_code, 404)
+
+        # permission error, deleting another user's tweet
+        response_incorrect_owner = client.delete('/api/tweets/3/delete/')
+        self.assertEqual(response_incorrect_owner.status_code, 404) 
+
+    
